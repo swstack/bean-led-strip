885f604c6924a6c98cfb06da707755b6901de17e
Stephen Stack <sstack@punchthrough.com>
2015-09-14 15:13:03 -0500

    fw: simple program that just writes to Tx when it gets Rx

--- firmware/bean_led_strip/bean_led_strip.ino
+++ firmware/bean_led_strip/bean_led_strip.ino
@@ -1,8 +1,18 @@
+
+
 void setup() {
-    
+    Serial.begin(57600);
+    Serial.setTimeout(25);
+    Serial.println("Setup Complete");
 }
 
 void loop() {
+    char buffer[10];
+    size_t length = 10;
+    length = Serial.readBytes(buffer, length);
 
-
+    if (length > 0) {
+        Serial.println("Got Data");
+    }
+    delay(1000);
 }
