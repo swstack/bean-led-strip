3ad57ac30797f139d52b38fa5c94c84fb0d18de0
Stephen Stack <sstack@punchthrough.com>
2016-02-04 17:34:21 -0600

    fw: setup GPIO pins

--- firmware/bean_led_strip/bean_led_strip.ino
+++ firmware/bean_led_strip/bean_led_strip.ino
@@ -1,8 +1,18 @@
 
+#define PIN_RED 1
+#define PIN_GREEN 2
+#define PIN_BLUE 0
+
 
 void setup() {
     Serial.begin(57600);
     Serial.setTimeout(25);
+    pinMode(PIN_RED, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+    pinMode(PIN_BLUE, OUTPUT);
+    digitalWrite(PIN_RED, LOW);
+    digitalWrite(PIN_GREEN, LOW);
+    digitalWrite(PIN_BLUE, LOW);
     Serial.println("Setup Complete");
 }
 
@@ -14,5 +24,7 @@ void loop() {
     if (length > 0) {
         Serial.println("Got Data");
     }
+
+    digitalWrite(PIN_GREEN, HIGH);
     delay(1000);
 }
