1f1755ec9104d479897a20b302e0ba0738f71d3e
Stephen Stack <sstack@punchthrough.com>
2016-02-05 13:17:19 -0600

    fw: turn off lights if recieve command 0

--- firmware/bean_led_strip/bean_led_strip.ino
+++ firmware/bean_led_strip/bean_led_strip.ino
@@ -1,30 +1,40 @@
-
-#define PIN_RED 1
-#define PIN_GREEN 2
-#define PIN_BLUE 0
-
+#define PIN_RED 2
+#define PIN_GREEN 0
+#define PIN_BLUE 1
 
 void setup() {
-    Serial.begin(57600);
-    Serial.setTimeout(25);
+    Serial.begin();
+
     pinMode(PIN_RED, OUTPUT);
     pinMode(PIN_GREEN, OUTPUT);
     pinMode(PIN_BLUE, OUTPUT);
     digitalWrite(PIN_RED, LOW);
     digitalWrite(PIN_GREEN, LOW);
     digitalWrite(PIN_BLUE, LOW);
+
+    turnOff();
+    analogWrite(PIN_GREEN, 200);
+
     Serial.println("Setup Complete");
 }
 
 void loop() {
-    char buffer[10];
-    size_t length = 10;
-    length = Serial.readBytes(buffer, length);
+    char buffer[10] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    size_t bytesRead = Serial.readBytes(buffer, 10);
 
-    if (length > 0) {
-        Serial.println("Got Data");
+    if (bytesRead > 0) {
+        if (buffer[0] == 0) {
+          turnOff();
+        } else {
+          turnOff();
+          analogWrite(PIN_RED, 200);
+          Bean.sleep(1000);
+        }
     }
+}
 
-    digitalWrite(PIN_GREEN, HIGH);
-    delay(1000);
+void turnOff() {
+  digitalWrite(PIN_GREEN, LOW);
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_BLUE, LOW);
 }
